889463368c2fcb6e972e9bbdfd91cb90ec96ddfd
samuel1 <ekechukwusamuel15@gmail.com>
2022-08-19 12:40:30 +0100

    completed  foodplug project

---
+++ starter/main.js
@@ -0,0 +1,10 @@
+
+
+var hamburger = document.querySelector('.hamburger-menu')
+var mobilelinks = document.querySelector('.mobile_link-holder')
+var backdrop = document.querySelector('.backdrop')
+function Addclass() {
+    hamburger.classList.toggle('show')
+    mobilelinks.classList.toggle('showlinks')
+    backdrop.classList.toggle('showlinks')
+}
